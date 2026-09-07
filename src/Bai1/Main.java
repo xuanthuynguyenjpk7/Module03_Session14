70256c1aa6f068519a9772b616ba8eba1c179954
xuant <xuanthuynguyenjpk7@gmail.com>
2026-09-08 00:55:09 +0900

    add

---
+++ src/Bai1/Main.java
@@ -0,0 +1,13 @@
+package Bai1;
+
+import Bai1.StudentDAO;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StudentDAO studentDAO = new StudentDAO();
+
+        studentDAO.addStudents();
+    }
+}
